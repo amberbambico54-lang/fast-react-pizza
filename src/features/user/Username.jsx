a17803477a13e3ba8cbcbd455c0affde2c08f5ef
graziella01 <amberbambico.54@gmail.com>
2026-01-28 22:24:06 +0800

    feat: enhance layout and styling in CartOverview, Username, Header, and Home components

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,6 +1,6 @@
 function Username() {
     return (
-        <div className="hidden text-sm font-semibold">
+        <div className="hidden sm:block text-sm font-semibold">
            Freyan 
         </div>
     )
