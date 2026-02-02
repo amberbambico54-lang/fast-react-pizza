2731625d32f7ca04936f777cfaf5a7287f7a1735
graziella01 <amberbambico.54@gmail.com>
2026-02-02 21:51:50 +0800

    feat: add UpdateOrder component for updating order priority and integrate address fetching in CreateOrder

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,13 +1,14 @@
 import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getCart, getTotalCartPrice } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../store";
 import { clearCart } from "../cart/cartSlice";
 import { formatCurrency } from "../../utils/helpers";
 import { useState } from "react";
+import { fetchAddressThunk } from "../user/userSlice";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -20,9 +21,19 @@ function CreateOrder() {
   // const cart = fakeCart;
 
   const cart = useSelector(getCart);
-  const username = useSelector((state) => state.user.username);
-  const formErrors = useActionData();
 
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+
+  const isLoadingAddress = addressStatus === "loading";
+
+  const formErrors = useActionData();
+  const dispatch = useDispatch();
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "loading";
 
@@ -60,16 +71,38 @@ function CreateOrder() {
           </div>
         </div>
 
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
               className="input w-full"
               type="text"
               name="address"
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
             />
+
+            {addressStatus === "error" && (
+              <p className="mt-2 rounded bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
+          {!position.latitude && !position.longitude && (
+            <span className="absolute right-[3px] top-[3px] z-50 sm:right-[5px] sm:top-[5px]">
+              <Button
+                disabled={isLoadingAddress}
+                type="small"
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddressThunk());
+                }}
+              >
+                Get Position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className="mb-12 flex items-center gap-5">
@@ -88,10 +121,18 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting} type="primary">
-            {" "}
+          <input
+            type="hidden"
+            name="position"
+            value={
+              position.longitude && position.latitude
+                ? `${position.latitude},${position.longitude}`
+                : ""
+            }
+          />
+          <Button disabled={isSubmitting || isLoadingAddress} type="primary">
             {isSubmitting
-              ? "Placing Order"
+              ? "Placing Order...."
               : `Order now from ${formatCurrency(totalPrice)}`}
           </Button>
         </div>
@@ -116,7 +157,7 @@ export async function action({ request }) {
     errors.phone =
       "Please give us your correct phone number. We might need it to contact you.";
 
-  if (Object.keys(errors).lenght > 0) return errors;
+  if (Object.keys(errors).length > 0) return errors;
 
   //if everything is okey, create new order and redirect
   const newOrder = await createOrder(order);
--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -13,7 +13,7 @@ function SearchOrder() {
     navigate(`/order/${query}`);
     setQuery("");
   }
-useNavigate()
+
   return (
     <form onSubmit={handleSubmit}>
       <input
@@ -21,7 +21,7 @@ useNavigate()
         placeholder="Search orders"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className="rounded-full px-4 py-2 text-sm bg-yellow-100 placeholder:text-stone-400 w-28 sm:w-64 sm:focus:w-72 transition-all duration-300 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 "
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
       />
     </form>
   );
